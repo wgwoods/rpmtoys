9849fe5653c324c2dce34ba902022452fcb82774
Will Woods <wwoods@redhat.com>
2018-04-04 19:41:00 -0400

    rpmtoys.tags: improvements!
    We've now got complete lists of tags for DEPENDENCY, SCRIPTLET, etc,
    plus a list of tags that typically have one entry per file, plus some
    new TAG_GROUPS, etc. Heck yes.

--- rpmtoys/tags.py
+++ rpmtoys/tags.py
@@ -30,36 +30,77 @@ for t in info:
     if t.num not in bynum or len(bynum[t.num].name) < len(t.name):
         bynum[t.num] = t
 
+
 def getname(tagnum):
     if tagnum not in bynum:
         return str(tagnum)
     else:
         return bynum[tagnum].name
 
+
+def byprefix(pfx):
+    if type(pfx) == str:
+        return {t.num for t in info if t.name.startswith(pfx)}
+    else:
+        return {t.num for t in info if any(t.name.startswith(p) for p in pfx)}
+
 # --- Below here we have a bunch of tag metadata (meta-metadata?)
 
 BIN_TAGS = frozenset(t.num for t in info if t.vtype == 'BIN')
 SCALAR_TAGS = frozenset(t.num for t in info if t.rtype == 'SCALAR')
 
-# My own artisinal hand-crafted groupings for tags
-TAG_GROUPS = {
-    "CHANGELOG": frozenset({1080, 1081, 1082}),
-    "FILEDIGESTS": frozenset({1035}),
-    "FILENAMES": frozenset({1116, 1117, 1118}),
-    "FILESTAT": frozenset({1028, 1029, 1030, 1031, 1032, 1033, 1034, 1036,
-                           1039, 1040, 1095, 1096, 5010}),
-    "RPMFILEMD": frozenset({1045, 1097, 1098, 1099, 1140, 1037}),
-    "FILECLASS": frozenset({1141, 1142}),
-    "FILEDEPENDS": frozenset({1143, 1144, 1145}),
-    "BUILDMD": frozenset({1132, 1122, 1044, 1007, 1006, 1022, 1021}),
-    "DISTROMD": frozenset({1010, 1011, 1015}),
-    "PACKAGEINFO": frozenset({1004, 1005, 1000, 1014, 1016, 1020, 5034}),
-    # These two are incomplete at the moment
-    "DEPENDENCY": frozenset({1047, 1048, 1049, 1050, 1053, 1054, 1055, 1090}),
-    "SCRIPTLETS": frozenset({1065, 1066, 1067, 1068, 1069, 1085, 1086, 1087,
-                             1088, 1091, 1092, 1112, 1113, 1114, 1115, 1151,
-                             1152, 1153, 1154, 1023, 1024, 1025, 1026}),
+# Group dependencies by type. Note that the names are a _prefix_ for a bunch of
+# tags that get zipped together inside RPM to create each dependency "item".
+DEPENDENCY_GROUPS = {
+    "BASIC": ["Provide", "Require", "Conflict", "Obsolete"],
+    "SOFT": ["Enhance", "Recommend", "Suggest", "Supplement"],
+    "OLD": ["Oldenhance", "Oldsuggest"],
+    "SPECIAL": ["Order", "Patches"],
 }
+# Same deal as above, but for scriptlets.
+SCRIPTLET_GROUPS = {
+    "BASIC": ["Prein", "Postin", "Preun", "Postun", "Pretrans", "Posttrans"],
+    "TRIGGER": ["Trigger"],
+    "FILETRIGGER": ["Filetrigger", "Transfiletrigger"],
+    "VERIFY": ["Verify"],
+}
+
+# map each name "stem" to corresponding set of tag numbers
+DEPENDENCY_NAMES = {n: byprefix(n)
+                    for gn in DEPENDENCY_GROUPS.values() for n in gn}
+SCRIPTLET_NAMES = {n: byprefix(n)
+                   for sn in SCRIPTLET_GROUPS.values() for n in sn}
+
+# Lovingly handcrafted tag groupings.
+# TODO: Size (1009), Archivesize (1046), Longsize (5009)
+# TODO: Policy/Contexts junk
+TAG_GROUPS = {
+    "FILEDIGESTS": {1035},
+    "FILENAMES":   {1116, 1117, 1118},
+    "FILESTAT":    {1028, 1029, 1030, 1031, 1032, 1033, 1034, 1036, 1039, 1040,
+                    1095, 1096, 5010},
+    "FILECLASS":   {1141, 1142},
+    "FILEDEPENDS": {1143, 1144, 1145},
+    "RPMMD":       {1064, 1094, 5018, 5062},
+    "RPMFILEMD":   {1045, 1097, 1098, 1099, 1140, 1037},
+    "BUILDMD":     {1132, 1122, 1044, 1007, 1006, 1022, 1021, 1106},
+    "DISTROMD":    {1010, 1011, 1015, 1123, 1155},
+    "PACKAGEINFO": {1000, 1001, 1002, 1003, 1004, 1005, 1014, 1016, 1020,
+                    5012, 5034},
+    "PAYLOADMD":   {1124, 1125, 1126, 5092, 5093},
+    "IMAGES":      {1012, 1013, 1043},
+    "NEVRAS":      {1196, 5013, 5014, 5015, 5016},
+    "SRPM":        {1018, 1019, 1051, 1052, 1059, 1060, 1061, 1062, 1089},
+    "RPMDB":       {1008, 1127, 1128, 1129}, # TODO: there's definitely more
+    "DEPRECATED":  {1027, 1119, 1120, 1121, 5007},
+    "CHANGELOG":   byprefix("Changelog"),
+    "DEPENDENCY":  byprefix(DEPENDENCY_NAMES),
+    "SCRIPTLET":   byprefix(SCRIPTLET_NAMES),
+}
+
+PER_FILE_TAGS = {1028, 1030, 1033, 1034, 1035, 1036, 1037, 1039, 1040, 1045,
+                 1095, 1096, 1097, 1116, 1117}
+
 # Confirm that each tag only belongs to one group
 assert(len(set(i for s in TAG_GROUPS.values() for i in s)) ==
        len(list(i for s in TAG_GROUPS.values() for i in s)))
