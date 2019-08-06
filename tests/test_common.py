b98e7ce0ee625eb171cdf5d386948f451859ec2d
Will Woods <wwoods@redhat.com>
2019-08-06 17:41:26 -0400

    Minor tweak for test_common
    Tests might want to use files in TESTDIR that aren't in RPMDIR, so
    split them into separate variables.

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,6 +1,7 @@
 import os
 
-RPMDIR = os.path.join(os.path.dirname(__file__), "rpms")
+TESTDIR = os.path.dirname(__file__)
+RPMDIR = os.path.join(TESTDIR, "rpms")
 RPMFILE = {
     'fuse-common':os.path.join(RPMDIR, "fuse-common-3.5.0-1.fc30.x86_64.rpm"),
     'geronimo-jta':os.path.join(RPMDIR, "geronimo-jta-1.1.1-17.el7.noarch.rpm"),
